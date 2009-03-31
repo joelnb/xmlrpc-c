759f6a1d8ddd224f54421b6c712b59e082ab647c
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2009-03-31 03:29:44 +0000

    First 1.18 release
    git-svn-id: https://svn.code.sf.net/p/xmlrpc-c/code@1692 98333e67-4a24-44d7-a75c-e53540dd3050

---
+++ release_number/01.18.00/include/xmlrpc-c/server_pstream.hpp
@@ -0,0 +1,112 @@
+#ifndef SERVER_PSTREAM_HPP_INCLUDED
+#define SERVER_PSTREAM_HPP_INCLUDED
+
+#ifdef WIN32
+#include <winsock.h>  /* For XMLRPC_SOCKET (= SOCKET) */
+#endif
+
+#include <xmlrpc-c/config.h>  /* For XMLRPC_SOCKET */
+#include <xmlrpc-c/registry.hpp>
+#include <xmlrpc-c/packetsocket.hpp>
+
+namespace xmlrpc_c {
+
+class serverPstreamConn {
+
+public:
+
+    struct constrOpt_impl;
+
+    class constrOpt {
+    public:
+        constrOpt();
+        ~constrOpt();
+
+        constrOpt & registryPtr       (xmlrpc_c::registryPtr      const& arg);
+        constrOpt & registryP         (const xmlrpc_c::registry * const& arg);
+        constrOpt & socketFd          (XMLRPC_SOCKET  const& arg);
+
+    private:
+        struct constrOpt_impl * implP;
+        friend class serverPstreamConn;
+    };
+
+    serverPstreamConn(constrOpt const& opt);
+
+    ~serverPstreamConn();
+
+    void
+    runOnce(volatile const int * const interruptP,
+            bool *               const eofP);
+
+    void
+    runOnce(bool * const eofP);
+
+    void
+    runOnceNoWait(bool * const eofP,
+                  bool * const didOneP);
+
+    void
+    runOnceNoWait(bool * const eofP);
+
+    void
+    run(volatile const int * const interruptP);
+
+    void
+    run();
+
+private:
+    struct serverPstreamConn_impl * implP;
+};
+
+
+class serverPstream {
+
+public:
+
+    struct constrOpt_impl;
+
+    class constrOpt {
+    public:
+        constrOpt();
+        ~constrOpt();
+
+        constrOpt & registryPtr       (xmlrpc_c::registryPtr      const& arg);
+        constrOpt & registryP         (const xmlrpc_c::registry * const& arg);
+        constrOpt & socketFd          (XMLRPC_SOCKET  const& arg);
+
+    private:
+        struct constrOpt_impl * implP;
+        friend class serverPstream;
+    };
+
+    serverPstream(constrOpt const& opt);
+
+    ~serverPstream();
+
+    void
+    runSerial(volatile const int * const interruptP);
+
+    void
+    runSerial();
+
+    void
+    terminate();
+    
+    class shutdown : public xmlrpc_c::registry::shutdown {
+    public:
+        shutdown(xmlrpc_c::serverPstream * const severAbyssP);
+        virtual ~shutdown();
+        void doit(std::string const& comment, void * const callInfo) const;
+    private:
+        xmlrpc_c::serverPstream * const serverPstreamP;
+    };
+
+private:
+    struct serverPstream_impl * implP;
+};
+
+
+} // namespace
+
+#endif
