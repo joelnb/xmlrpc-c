ea5a160b665dc99d372edef74491db6d3a7a03af
giraffedata <giraffedata@98333e67-4a24-44d7-a75c-e53540dd3050>
2019-03-29 03:33:33 +0000

    Promote stable to super stable
    git-svn-id: https://svn.code.sf.net/p/xmlrpc-c/code@3033 98333e67-4a24-44d7-a75c-e53540dd3050

--- super_stable/version.mk
+++ super_stable/version.mk
@@ -7,5 +7,5 @@
 #
 ###############################################################################
 XMLRPC_MAJOR_RELEASE = 1
-XMLRPC_MINOR_RELEASE = 43
-XMLRPC_POINT_RELEASE = 8
+XMLRPC_MINOR_RELEASE = 51
+XMLRPC_POINT_RELEASE = 3
